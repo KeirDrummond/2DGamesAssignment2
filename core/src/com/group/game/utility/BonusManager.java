a8ddf9e4b4513ccc9ddfa492712402d291af8bd5
KeirDrummond <k.drummond2@uni.brighton.ac.uk>
2019-03-21 16:07:17 +0000

    Collision branch

--- core/src/com/group/game/utility/BonusManager.java
+++ core/src/com/group/game/utility/BonusManager.java
@@ -1,4 +1,39 @@
-package com.group.game.utility;
+/*package com.group.game.utility;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.group.game.bodies.PlayerCharacter;
+
+import static com.group.game.utility.Constants.TINY;
+
+/* Uses references that will point to the assignment 1 work.
+* Will not function correctly until the proper references are fixed.
+*/
+
+/*
 
 public class BonusManager {
+    PowerUpSprite[] bonusCollection = new PowerUpSprite[MAX_BONUS_SPRITES];
+    PlayerCharacter playerCharacter;
+    private int bonusSpriteToDisplay;
+    private float timeCount;
+    private float timeToNextBonus = MAX_TIME_TO_NEXT_BONUS;
+    public static boolean handlingCollision = false;
+    private Vector2 previousBonusPosition;
+
+    public BonusManager(PlayerCharacter playerCharacter){
+        this.playerCharacter = playerCharacter;
+        for(int i=0; i<bonusCollection.length;i++) {
+            bonusCollection[i] = new PowerUpSprite(POWER_UP_ATLAS_PATH, TINY);
+        }
+    }
+
+    public void draw(SpriteBatch batch){
+        for(int i=0;i<bonusCollection.length;i++){
+            if(bonusColleciton[i].isDisplayed()){
+                bonusCollection[i].draw(batch);
+            }
+        }
+    }
 }
+*/
