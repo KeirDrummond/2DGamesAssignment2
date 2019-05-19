59cb3006d857614021d6a72c56d9055a0c97e293
KeirDrummond <k.drummond2@uni.brighton.ac.uk>
2019-05-19 15:25:04 +0100

    Initial commit

--- core/src/com/group/game/utility/BonusManager.java
+++ core/src/com/group/game/utility/BonusManager.java
@@ -25,7 +25,6 @@ import static com.group.game.utility.Constants.TILE_SIZE;
 
 /* Uses references that will point to the assignment 1 work.
 * Will not function correctly until the proper references are fixed.
-*
 */
 
 public class BonusManager {
