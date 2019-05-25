135bc0de910443cb576bdbf3e588489466577382
JJAMitchell <43954591+JJAMitchell@users.noreply.github.com>
2019-05-25 18:46:20 +0100

    Add files via upload
    Added paths to the sound files

--- core/src/com/group/game/utility/Constants.java
+++ core/src/com/group/game/utility/Constants.java
@@ -43,7 +43,7 @@ public class Constants {
     public static float PLAYER_OFFSET_Y=2.15f;
     public static float PLAYER_OFFSET_X=1.5f;
     //player graphics
-    public static final String PLAYER_ATLAS_PATH = "atlas/smurf_assets.atlas";
+    public static final String PLAYER_ATLAS_PATH = "atlas/character.atlas";
     public static final Texture MEDIUM = new Texture(Gdx.files.internal("gfx/mediumSize.png"));
     public static final Texture SMALL = new Texture(Gdx.files.internal("gfx/smallSize.png"));
     public static final Texture TINY = new Texture(Gdx.files.internal("gfx/tinySize.png"));
@@ -54,6 +54,12 @@ public class Constants {
 
     public static final String REVOLVER_AMMO_PICKUP_PATH = "atlas/revolver_ammo.atlas";
     public static final String REVOLVER_AMMO_PICKUP_SOUND_PATH = "sounds/revolverpickup.wav";
+
+    public static final String SOUNDTRACK_PATH = "sounds/soundtrack.wav";
+    public static final String GUNSHOT_PATH = "sounds/gunshot.wav";
+    public static final String PICKUP_PATH = "sounds/pickup.wav";
+    public static final String HIT_PATH = "sounds/hit_hurt.wav";
+    public static final String JUMP_PATH = "sounds/jump.wav";
 
 
 }
