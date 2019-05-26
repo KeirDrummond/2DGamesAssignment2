e04e1154cedfed26fa32b8f02b4ac664116e5ace
KeirDrummond <k.drummond2@uni.brighton.ac.uk>
2019-05-26 16:58:18 +0100

    revolver ammo pointer added

--- android/assets/tileData/PowerUps.tsx
+++ android/assets/tileData/PowerUps.tsx
@@ -1,10 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="PowerUps" tilewidth="96" tileheight="96" tilecount="1" columns="0">
+<tileset version="1.2" tiledversion="1.2.4" name="PowerUps" tilewidth="128" tileheight="128" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
    <property name="Name" value="Moonshine"/>
   </properties>
   <image width="96" height="96" source="pointers/sprite_0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="pointers/RevolverAmmo.png"/>
  </tile>
 </tileset>
